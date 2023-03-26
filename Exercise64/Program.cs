04d6e8e1417b407c74cf817b168a23ff56569f0e
Kciraf <valsinat44a@mail.ru>
2023-03-26 17:31:42 +0600

    Added exercise 64

--- Exercise64/Program.cs
+++ Exercise64/Program.cs
@@ -3,3 +3,21 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+void NumPrint(int num)
+{
+    if (num >= 1)
+    {
+        System.Console.Write($"{num} ");
+        num -= 1;
+        NumPrint(num);
+    }
+}
+
+
+System.Console.WriteLine("ВВедите число больше 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n >= 1)
+{
+    NumPrint(n);
+}
+else System.Console.WriteLine("В данном промежутке нет натуральных чисел");
