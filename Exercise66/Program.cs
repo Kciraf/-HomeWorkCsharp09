7baaa46e6a6b2f961311693d96efea107bee980e
Kciraf <valsinat44a@mail.ru>
2023-03-26 17:51:06 +0600

    Added exercise 66

--- Exercise66/Program.cs
+++ Exercise66/Program.cs
@@ -3,3 +3,27 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+int NumSegmentSum(int start, int end)
+{
+    int result = 0;
+    if (start <= end)
+    {
+        return result += start + NumSegmentSum(start + 1, end);
+    }
+    return result;
+}
+
+System.Console.WriteLine("Введите число начало вывода: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число конца вывода: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int temp = 0;
+
+if (m > n)
+{
+    temp = m;
+    m = n;
+    n = temp;
+}
+
+System.Console.WriteLine("Сумма чисел в данном промежутке: " + NumSegmentSum(m, n));
